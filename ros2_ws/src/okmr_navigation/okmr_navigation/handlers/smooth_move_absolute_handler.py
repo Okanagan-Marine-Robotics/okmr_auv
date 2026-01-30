1d4e34b56e840430237d3e3d408ed4e21fde8d6a
Patrik Balazsy <patrikpbalazsy@gmail.com>
2026-01-30 08:05:52 +0000

    Modified smooth move to take in real pose data and set up a potential test script

--- ros2_ws/src/okmr_navigation/okmr_navigation/handlers/smooth_move_absolute_handler.py
+++ ros2_ws/src/okmr_navigation/okmr_navigation/handlers/smooth_move_absolute_handler.py
@@ -1,15 +1,14 @@
 import numpy as np
 from scipy.interpolate import CubicSpline
-from okmr_msgs.msg import GoalPose
-from okmr_msgs.srv import DistanceFromGoal
-from okmr_navigation.handlers.freeze_handler import execute_freeze
+import rclpy
+
+from okmr_msgs.msg import GoalPose, ControlMode
+from okmr_msgs.srv import GetPoseTwistAccel
 from okmr_navigation.navigator_action_server import NavigatorActionServer
 from okmr_navigation.handlers.movement_execution_common import (
     execute_movement_with_monitoring,
-    execute_test_movement_common,
 )
 from okmr_navigation.handlers.set_control_mode import set_control_mode
-from okmr_msgs.msg import ControlMode
 
 # Mirror of move_absolute_handler.py (Wrapper)
 
@@ -19,36 +18,74 @@ def handle_smooth_move_absolute(goal_handle):
     
     set_control_mode(ControlMode.POSE)
     node = NavigatorActionServer.get_instance()
+    
+    # Define target acceptance radius and goal request for consistency
+    command_msg = goal_handle.request.command_msg
+    total_timeout = command_msg.timeout_sec
+    radius = command_msg.radius_of_acceptance
+    
+    
 
     waypoints = _generate_smooth_waypoints(goal_handle, node)
+    
+    last_result = Movement.Result()
+    last_result.debug_info = "No waypoints generated: Sensors not ready"
 
     # MVP reuse logic for each point
     last_result = None
+    num_wp = len(waypoints)
+    
     for i, waypoint_pose in enumerate(waypoints):
         node.get_logger().info(f"Moving smoothly to waypoint {i+1}/{len(waypoints)}")
 
-        goal_handle.request.command_msg.goal_pose.pose = waypoint_pose
+        command_msg.goal_pose.pose = waypoint_pose
+        
+        command_msg.timeout_sec = total_timeout / num_wp
+        command_msg.radius_of_acceptance = radius
 
         last_result = execute_movement_with_monitoring(
             goal_handle, _publish_pose_goal, "distance_from_pose_goal"
         )
 
         if not goal_handle.is_active:
+            node.get_logger().warn("Smooth movement disengaged")
             break
 
-        return last_result
+    return last_result
 
 
-def _generate_smooth_waypoints(goal_handle, node, num_waypoints = 10):
+def _generate_smooth_waypoints(goal_handle, node, num_waypoints = 9):
 
     """
     Generates a cubic spline between the current position and the target
 
     """
-    from okmr_msgs.srv import GetPoseTwistAccel # Post MVP input
-
-    # Place holder for starting coords
-    start_pos = [0.0, 0.0, 0.0]
+    # Cubic spline now generates between the real AUV and target position
+    # Use a client to get AUV position
+    
+    client = node.create_client(GetPoseTwistAccel, "/get_pose_twist_accel")
+    
+    if not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().error("Unable to get pose information, going to default origin pos")
+        start_pos = [0.0, 0.0, 0.0]
+    else:
+        request = GetPoseTwistAccel.Request()
+        future = client.call_async(request)
+        
+        # Apply splin until complete to avoid a deadlocked execution!
+        
+        rclpy.spin_until_future_complete(node, future, timeout_sec=1.0)
+        
+        if future.done() and future.result().success:
+            res = future.result()
+            start_pos = [
+                res.pose.position.x,
+                res.pose.position.y,
+                res.pose.position.z
+            ]
+        else:
+            node.get_logger().error("Nav sensors not ready, aborting mission")
+            return []
 
     target_pose = goal_handle.request.command_msg.goal_pose.pose
     end_pos = [target_pose.position.x, target_pose.position.y, target_pose.position.z]
@@ -56,7 +93,7 @@ def _generate_smooth_waypoints(goal_handle, node, num_waypoints = 10):
     # Create an intermediate point in the spline
 
     mid_pos = [(s + e) / 2.0 for s, e in zip(start_pos, end_pos)]
-    mid_pos[2] += 0.5 # Verticle curve adjustment
+    mid_pos[2] += 0.3 # Verticle curve adjustment
     
     points = np.array([start_pos, mid_pos, end_pos])
     t = np.linspace(0, 1, len(points))
