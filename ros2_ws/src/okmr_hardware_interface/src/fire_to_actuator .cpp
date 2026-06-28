44592f20667f64c85b37b0abd792192ddd94cc30
HakimRashid <rashidhakim514@gmail.com>
2026-06-28 12:20:29 -0700

    finished torpedo commands.

--- ros2_ws/src/okmr_hardware_interface/src/fire_to_actuator .cpp
+++ ros2_ws/src/okmr_hardware_interface/src/fire_to_actuator .cpp
@@ -1,33 +1,16 @@
-#include <cmath>
-#include <lazycsv.hpp>
-#include <okmr_msgs/msg/battery_voltage.hpp>
 #include <okmr_msgs/msg/actuator_command.hpp>
 #include <okmr_msgs/msg/fire_torpedo_command.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <stdexcept>
-#include <string>
 
-class FireToActuator : public rclcpp::Node {
+u_int8_t NUM_ACTUATORS = 2;
+
+class FireToActuatorNode : public rclcpp::Node {
     public:
-    FireToActuator () : Node ("actuator_to_fire") {
-        this->declar_parameter<std::string> ("actuator_curve_file", "");
-
-        std::string csv_file_path = this->get_parameter ("actuator_curve_file").as_string();
-        if(csv_file_path.empty()) {
-            RCLCPP_ERROR (this->get_logger (), "actuator_curve_file parameter not set");
-            return;
-        }
-
-        load_actuator_curve (csv_file_path);
-
-        fire_torpedo_sub = this->create_subscription<okmr_msgs::msg::FireTorpedoCommand> (
+    FireToActuatorNode () : Node ("actuator_to_fire") {
+        fire_torpedo_sub_ = this->create_subscription<okmr_msgs::msg::FireTorpedoCommand> (
             "/fire_torpedo_command", 10,
-            std::bind (&FireToActuator::fire_torpedo_callback, this, std::placeholders::_1)
-        );
-
-        voltage_sub_ = this->create_subscription<okmr_msgs::msg::BatteryVoltage> (
-            "/voltage", 10,
-            std::bind (&FireToActuator::voltage_callback, this, std::placeholders::_1)
+            std::bind (&FireToActuatorNode::fire_torpedo_callback, this, std::placeholders::_1)
         );
 
         actuator_pub_ = this->create_publisher<okmr_msgs::msg::ActuatorCommand> (
@@ -36,11 +19,45 @@ class FireToActuator : public rclcpp::Node {
 
         RCLCPP_INFO (this->get_logger (), "FireToActuator node initialized");
     }
-
-    std::map<float, std::map<float>> actuator_curve_map_;
-    float current_voltage_ = 16.0f;
+    private:
 
     rclcpp::Subscription<okmr_msgs::msg::FireTorpedoCommand>::SharedPtr fire_torpedo_sub_;
-    rclcpp::Subscription<okmr_msgs::msg::BatteryVoltage>::SharedPtr voltage_sub_;
-    
+    rclcpp::Publisher<okmr_msgs::msg::ActuatorCommand>::SharedPtr actuator_pub_;
+
+    void fire_torpedo_callback (const okmr_msgs::msg::FireTorpedoCommand::SharedPtr msg) {
+        auto actuator_msg = std::make_shared<okmr_msgs::msg::ActuatorCommand> ();
+        actuator_msg->state = true;
+
+        u_int8_t tube_number = msg->tube_number;
+        if (tube_number > NUM_ACTUATORS) {
+            RCLCPP_ERROR (this->get_logger (), "Failed to fire torpedo, invalid tube number: %d", tube_number);
+            hrow std::runtime_error("Invalid tube number requested!");
+        }
+        if (tube_number < NUM_ACTUATORS) {
+            actuator_msg->index = msg->tube_number;
+            actuator_pub_->publish (*actuator_msg);
+            return;
+        }
+        for (u_int8_t i = 0; i < NUM_ACTUATORS; ++i) {
+            actuator_msg->index = i;
+            actuator_pub_->publish (*actuator_msg);
+        }
+        RCLCPP_DEBUG (this->get_logger (), "Published actuator commands");
+    }
+};
+
+int main (int argc, char** argv) {
+    rclcpp::init (argc, argv);
+
+    try {
+        auto node = std::make_shared<FireToActuatorNode> ();
+        rclcpp::spin (node);
+    } catch (const std::exception& e) {
+        RCLCPP_ERROR (rclcpp::get_logger ("fire_to_actuator"), "Node failed: %s", e.what ());
+        rclcpp::shutdown ();
+        return 1;
+    }
+
+    rclcpp::shutdown ();
+    return 0;
 }
