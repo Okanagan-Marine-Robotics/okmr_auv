9bc2233a8240e43bbf0eb09f53edefd4e2033454
Isaac Kohler-Mergel <isaackm100@gmail.com>
2026-07-02 07:59:16 -0700

    Arm action and service

--- ros2_ws/src/okmr_hardware_interface/okmr_hardware_interface/arm_control_server.py
+++ ros2_ws/src/okmr_hardware_interface/okmr_hardware_interface/arm_control_server.py
@@ -16,8 +16,15 @@ from rclpy.node import Node
 from rclpy.action import ActionServer
 from okmr_msgs.msg import ServoCommand
 from okmr_msgs.action import Arm
+from okmr_msgs.srv import ArmTarget
+import time
+
+SERVO_MOVE_RATE = 60.0 #deg/s, placeholder value for now, need to find actual servo spec
 
 class ArmActionServer(Node):
+
+    default_poses = [0, 0]   #Default angles the arm should assume at initial power on
+    current_poses = [0, 0]  #For tracking the current joint positions
 
     def __init__(self):
         super.__init__('arm_action_server')
@@ -26,10 +33,48 @@ class ArmActionServer(Node):
             Arm,
             'arm_movement',
             self.arm_callback)
+        self.cli = self.create_client(ArmTarget, 'arm_kinematics')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('kinematics service not available, waiting again...')
+        self.req = ArmTarget.Request()
 
 
-    def arm_callback(self, goal):
-        pass #TODO
+    async def arm_callback(self, goal):
+        target = goal.request.targetPose
+        gripper_state = goal.request.gripperState
+
+        self.req.targetPose = target
+        self.req.gripperState = gripper_state
+
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        joints = self.future.result().joint_poses
+
+        max_ang_dif = 0.0
+        for i in range(len(joints)):
+            max_ang_dif = max(max_ang_dif,abs(self.current_poses[i]-joints[i]))
+        
+        movement_time = max_ang_dif/SERVO_MOVE_RATE
+
+        feedback_msg = Arm.Feedback()
+        feedback_msg.status = 0
+        goal.publish_feedback(feedback_msg)
+
+        time.sleep(movement_time)
+
+        for i in range(len(joints)):
+             self.current_poses[i] = joints[i]
+        
+        result = Arm.Result()
+        result.exitStatus = 0
+        return result
+             
+
+
+
+
+
+
 
 def main(args=None):
     try:
--- ros2_ws/src/okmr_hardware_interface/okmr_hardware_interface/kinematics.py
+++ ros2_ws/src/okmr_hardware_interface/okmr_hardware_interface/kinematics.py
@@ -13,27 +13,51 @@ Current State:
 import rclpy
 from rclpy.node import Node
 from okmr_msgs.srv import ArmTarget
+import math
 
 ### Arm Model Constants
-
+SERVO_TRAVEL = 180 #Servo range of motion
+MOVEMENT_RATIO_ARM = 1.0 #mm movement in rack/deg rotation of the pinion attached to the servo, obtained from drawings
+GRIPPER_FULL_OPEN = 0 #Servo position where the gripper is open, assumed to be default position
+GRIPPER_FULL_CLOSE = 180 #Servo position where the gripper is closed
 
 class KinematicsService(Node):
 
     def __init__(self):
         super().__init__("arm_kinematics_service")
+        self.max_reach = SERVO_TRAVEL * MOVEMENT_RATIO_ARM
         self.srv = self.create_service(ArmTarget, 'arm_ik_service',self.arm_target_callback)
 
     def arm_target_callback(self,request, response):
-        pass #TODO
+        target = request.targetPose
+        grip_angle = GRIPPER_FULL_OPEN
+        if request.gripperState == ArmTarget.CLOSED:
+            grip_angle = GRIPPER_FULL_CLOSE
+        
+        target_dist = math.sqrt(target.x**2+target.y**2+target.z**2) #Currently assuming millimeters for target units
+        in_reach = target_dist<= self.max_reach
+        arm_angle = 0
+        if not in_reach:
+            arm_angle = SERVO_TRAVEL #Put the arm at full extension
+            self.get_logger.info("Requested arm position exceeds maximum reach")
+        else:
+            arm_angle = target_dist/MOVEMENT_RATIO_ARM
+        
+        response.joint_poses = [arm_angle, grip_angle]
+        response.inReach = in_reach
+
+        return response
+
+        
 
 
-def main():
+def main(): #TODO add entry points for client and server
 
     try:
         with rclpy.init():
             kin_service = KinematicsService()
             rclpy.spin(kin_service)
-            
+
     except KeyboardInterrupt:
         pass
 
