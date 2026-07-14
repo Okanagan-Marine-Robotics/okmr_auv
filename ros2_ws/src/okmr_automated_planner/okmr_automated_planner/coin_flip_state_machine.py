d6e3998dcd2bcf7caa6cca3c7441d9664d603780
Patrik Balazsy <patrikpbalazsy@gmail.com>
2026-07-13 21:44:38 -0700

    Initialize coin flip state machine with comments
    Add initial structure and comments for coin flip state machine.

--- ros2_ws/src/okmr_automated_planner/okmr_automated_planner/coin_flip_state_machine.py
+++ ros2_ws/src/okmr_automated_planner/okmr_automated_planner/coin_flip_state_machine.py
@@ -1,1 +1,11 @@
+#Import Statements
 
+
+#Define States
+
+# Unitialized, Initialized, Heads, Tails, Other
+
+# Define logic for each state
+
+
+# Make neccesarry calls for publishers, subscribers, logging, and other
