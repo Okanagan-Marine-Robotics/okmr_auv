0d292a4e9b3d225b7d93b2bb9507edb8ac56e063
CMIT amber <cmitamber@macbook-pro-5.tail9ed5f.ts.net>
2026-07-15 13:06:29 -0700

    temp

--- ros2_ws/src/okmr_navigation/okmr_navigation/handlers/bbl_dvl_handler.py
+++ ros2_ws/src/okmr_navigation/okmr_navigation/handlers/bbl_dvl_handler.py
@@ -1,5 +1,5 @@
 from okmr_msgs.action import Movement
-from okmr_msgs.msg import GoalPose, MovementCommand
+from okmr_msgs.msg import GoalPose, MovementCommand, GoalVelocity
 from okmr_msgs.msg._dvl import Dvl
 from okmr_navigation.handlers.get_pose_twist_accel import get_current_pose, get_current_twist
 from okmr_navigation.navigator_action_server import NavigatorActionServer
@@ -7,6 +7,8 @@ from okmr_navigation.handlers.set_velocity_handler import *
 from okmr_navigation.handlers.freeze_handler import *
 
 DIFF_ALLOWED = 0.3
+FRONT_IDX = [0, 3]
+BACK_IDX = [1, 2]
 
 def handle_bbl_dvl(goal_handle):
     node = NavigatorActionServer.get_instance()
@@ -14,7 +16,7 @@ def handle_bbl_dvl(goal_handle):
     dvl_sub = None
     
     try:
-        dvl_sub = node.create_subscription(Dvl, '/dvl', lambda msg: '_dvl_callback', 10)
+        dvl_sub = node.create_subscription(Dvl, '/dvl', _dvl_callback, 10)
             # should probably add a destroy somewhere - and uncache the sub above idk
     finally:
         pass
@@ -23,34 +25,32 @@ def _dvl_callback(msg):
     beams = msg.beam_distances
     node = NavigatorActionServer.get_instance()
 
-    front_idx = [0, 1]
-    back_idx = [2, 3]
     
     front_measurements = []
     back_measurements = []
     for i in front_idx:
-        front_measurements.append[beams[i]]
+        front_measurements.append(beams[i])
     for i in back_idx:
-        back_measurements.append[beams[i]]
+        back_measurements.append(beams[i])
     
-    if(min[front_measurements] < min[back_measurements]):
+    if(min(front_measurements) < min(back_measurements)):
         _send_velocity()
         node.get_logger().debug("Front aligned against wall. - 0.1m/s velocity")
         return
             
-    if(back_measurements[0] - back_measurements[1] <= DIFF_ALLOWED):
+    if(back_measurements[0] - back_measurements[1] >= DIFF_ALLOWED):
         _send_velocity()
         node.get_logger().debug("Diff between beams is too large. - 0.1m/s velocity")
         return
-    msg = MovementCommand();
-    msg.command = MovementCommand.FREEZE
-    handle_freeze(msg)
+    #msg = MovementCommand();
+    #msg.command = MovementCommand.FREEZE
+    execute_freeze()
     node.get_logger().debug("sub aligned - sent freeze pose")
 
         
 def _send_velocity():
     node = NavigatorActionServer.get_instance()
-    msg = MovementCommand();
+    msg = MovementCommand()
     msg.command = MovementCommand.SET_VELOCITY
-    msg.goal_velocity.twist.linear.z = 0.1
+    msg.goal_velocity.twist.angular.z = 0.1
     handle_set_velocity(msg)
